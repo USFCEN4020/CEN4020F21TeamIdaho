75a8a7d5c3d17a9f04152fe9adc4e009e660f201
Anton <44649959+amemov@users.noreply.github.com>
2021-09-26 11:57:26 -0400

    Add files via upload
    Epic #2 edits: user first & last name, create a job post, 'success' story on the main screen, 'people you may know',

---
+++ Database.py
@@ -0,0 +1,27 @@
+#SQLite 3 page 
+
+import sqlite3
+
+with sqlite3.connect("User.db") as db:
+    cursor = db.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS user(
+userID INTEGER PRIMARY KEY,
+username   VARCHAR(20) NOT NULL,
+password   VARCHAR(20) NOT NULL,
+first_name VARCHAR(20) NOT NULL,
+last_name  VARCHAR(20) NOT NULL); 
+""")
+
+###################################################
+# run this file if you want to empty the database #
+###################################################
+
+db.commit()
+
+#enter SQL queries here
+cursor.execute("DELETE FROM user")
+
+db.commit()
+print(cursor.fetchall())
